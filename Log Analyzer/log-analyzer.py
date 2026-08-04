5c743f048e4fa33a9881cf97132d37d012402a9b
Your Name <you@example.com>
2026-08-04 02:19:01 +0100

    merge changes

--- Log Analyzer/log-analyzer.py
+++ Log Analyzer/log-analyzer.py
@@ -8,26 +8,30 @@ def log_analyzer(logfilepath):
                 if ip_address in ip_address_failed:
                     ip_address_failed[ip_address] = ip_address_failed[ip_address] + 1
                 else:
-                    ip_address_failed[ip_address]= 1
+                    ip_address_failed[ip_address] = 1
     return ip_address_failed
 
 
 def main():
     print("---LOG ANALYZER---")
     logfilepath = input("Enter the path to the log file:").strip()
-    counts = log_analyzer(logfilepath)
+    count_failed = log_analyzer(logfilepath)
 
     print("IP addresses with failed login attempts:")
-    for ip_address in counts:
-        print(f"{ip_address} has {counts[ip_address]} failed login attemps.")
+    for ip_address in count_failed:
+        print(f"{ip_address} has {count_failed[ip_address]} failed login attemps.")
 
     print("Suspicious IP addresses that have more than 3 failed login attempts:")
     suspicious_found = False
-    for ip_address in counts:
-        if counts[ip_address] >= 3:
-            print(f"{ip_address} has {counts[ip_address]} failed login attemps and is flagged for suspicious activity")
+    for ip_address in count_failed:
+        if count_failed[ip_address] >= 3:
+            print(f"{ip_address} has {count_failed[ip_address]} failed login attemps and is flagged for suspicious activity")
             suspicious_found = True
-    if suspicious_found == False: 
+    if suspicious_found == False:
         print("No IP addresses were flagged for suspicious activity ")
+
+
+if __name__ == "__main__":
+    main()
 
 
